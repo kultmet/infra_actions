369a86bedf8d8b9348e892b8df1abf27be8cff03
Maystrenko Dmitriy <kultmet@gmail.com>
2023-01-05 16:36:43 +0300

    Flake8 and Django-tests

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'infra_app.apps.InfraAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'infra_app.apps.InfraAppConfig',
 ]
 
 MIDDLEWARE = [
