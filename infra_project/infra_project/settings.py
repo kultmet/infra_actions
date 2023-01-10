defd867430bcba4cb6451826393c026abaa6635f
Maystrenko Dmitriy <kultmet@gmail.com>
2023-01-10 12:47:28 +0300

    Пробуем боевой сервер. переустановил ALLOWED_HOSTS.

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SERVER_NAME = os.getenv('SERVER_NAME','localhost')
+# SERVER_NAME = os.getenv('SERVER_NAME', 'localhost')
 
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['localhost', ]
 
 
 # Application definition
