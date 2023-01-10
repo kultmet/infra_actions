a204d90edc1f3092cf9da84998b960d3e0390658
Maystrenko Dmitriy <kultmet@gmail.com>
2023-01-10 13:12:52 +0300

    Меняем контент главной страници.

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Какой же я молодец!!!')
 
 
 def second_page(request):
